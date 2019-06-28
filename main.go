f923ded43b2de90ffe81c25d597fdb49a7ad81b9
Jeremy Mouton <moutonjeremy@labbs.fr>
2019-06-28 19:01:49 +0200

    bump version

--- main.go
+++ main.go
@@ -18,7 +18,7 @@ func main() {
 	app.Name = "pm2-exporter"
 	app.Flags = settings.NewContext()
 	app.Action = run
-	app.Version = "v1.1.1"
+	app.Version = "v1.1.2"
 
 	err := app.Run(os.Args)
 	if err != nil {
